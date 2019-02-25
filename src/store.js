81afff3cfe5ee33cdd185bfe4bffaf004a3f40ce
Choe <Kevin.Choe@encana.com>
2019-02-25 13:37:09 -0700

    Vuex lesson 3 half-way

--- src/store.js
+++ src/store.js
@@ -4,7 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Kevin Choe' },
+    categories: [
+      'Sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {}
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was crated by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    localComputed() {
+      return something
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
