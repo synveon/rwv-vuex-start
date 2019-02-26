e933a60357c22a9e48a403ef511f3a1110c354d4
Choe <Kevin.Choe@encana.com>
2019-02-25 17:12:49 -0700

    Finshed State & Getters

--- src/store.js
+++ src/store.js
@@ -14,8 +14,30 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
     ]
   },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    // passing in a getter object
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    // dynamic getter example
+    getTodoById: state => id => {
+      return state.todos.find(todo => todo.id === id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,20 +2,20 @@
   <div>
     <h1>Create Event, {{ user.name }}</h1>
     <p>This event was crated by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
+    <p>{{ getTodoById(1) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   computed: {
-    localComputed() {
-      return something
-    },
+    ...mapGetters(['catLength', 'getTodoById']),
     ...mapState(['user', 'categories'])
   }
 }
